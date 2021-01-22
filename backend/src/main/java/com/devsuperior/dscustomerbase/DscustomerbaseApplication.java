9dea8cdc9ed9a67e1587f48bd6d7385b4839795a
johnyguido <johnyguido@hotmail.com.br>
2021-01-22 00:08:50 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscustomerbase/DscustomerbaseApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscustomerbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscustomerbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscustomerbaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscustomerbase/DscustomerbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscustomerbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscustomerbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
